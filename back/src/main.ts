c10fc4677f3c14f69f3d9678e36051d8b53e6721
Kim-Jin-Uk <gottkfthf@gmail.com>
2022-04-20 14:09:00 +0900

    setting hot reloading

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.SERVER_PORT || 3000;
+  await app.listen(port);
+  console.log(`server start on port: ${port}`);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
