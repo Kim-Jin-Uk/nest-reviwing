a4b411bbf0101ffde0209d2473cd93d41f387884
Kim-Jin-Uk <gottkfthf@gmail.com>
2022-04-20 15:54:34 +0900

    create logger middleware, setting morgan

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MorganInterceptor, MorganModule } from 'nest-morgan';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  //forRoot는 설정을 넣는 부분
+  //configModule - .env와 같은 역할
+  //morganModule - 로거 모듈
+  imports: [ConfigModule.forRoot({ isGlobal: true }), MorganModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    ConfigService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: MorganInterceptor('combined'),
+    },
+  ],
 })
 export class AppModule {}
--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
+//요청과 응답에 대해서 모름 -> 재사용성, 독립적 - 테스트시 따로 모킹을 해줄 필요가없음
 @Injectable()
 export class AppService {
+  //외부 configService 가져오기 - Nest가 관리하게끔
+  constructor(private readonly configService: ConfigService) {}
   getHello(): string {
-    return 'Hello World!';
+    return this.configService.get('COOKIE_SECRET');
+  }
+  postHello(): string {
+    return 'Hello Post!';
   }
 }
