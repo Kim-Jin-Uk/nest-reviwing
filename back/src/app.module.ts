a3583aa9ed4f67e59abc4f7ee584074a0b13532b
Kim-Jin-Uk <gottkfthf@gmail.com>
2022-04-21 10:50:51 +0900

    create module, controller, service - users, workspaces, channels, dms

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -4,12 +4,23 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MorganInterceptor, MorganModule } from 'nest-morgan';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { ChannelsModule } from './channels/channels.module';
+import { DmsModule } from './dms/dms.module';
 
 @Module({
   //forRoot는 설정을 넣는 부분
   //configModule - .env와 같은 역할
   //morganModule - 로거 모듈
-  imports: [ConfigModule.forRoot({ isGlobal: true }), MorganModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MorganModule,
+    UsersModule,
+    WorkspacesModule,
+    ChannelsModule,
+    DmsModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
